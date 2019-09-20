c0cdc86f52a3c52f9ba85d3ea056368909cbec2d
IROpen <hamid.6.11.79@gmail.com>
2019-09-20 15:40:56 +0430

    incClick owner

--- django/yektanet/advertiser_mangement/models.py
+++ django/yektanet/advertiser_mangement/models.py
@@ -5,13 +5,19 @@ class Advertiser(models.Model):
   clicks = models.IntegerField(default=0)
   views  = models.IntegerField(default=0)
 
+  def incClicks(self):
+    self.clicks += 1
+    self.save()
+
 class Ad(models.Model):
   title  = models.CharField(max_length=100, null = False)
   imgUrl = models.CharField(max_length=1000, null = False)
   link   = models.CharField(max_length=1000, null = False)
-  owner  = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+  owner  = models.ForeignKey(Advertiser, null = False, on_delete=models.CASCADE)
   clicks = models.IntegerField(default=0)
   views  = models.IntegerField(default=0)
+  
   def incClicks(self):
     self.clicks += 1
+    self.owner.incClicks()
     self.save()
