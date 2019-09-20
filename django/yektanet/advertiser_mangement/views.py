6ba72f1ee0497c6f2240094380cad90c5d490d80
IROpen <hamid.6.11.79@gmail.com>
2019-09-20 14:22:48 +0430

    send data to template

--- django/yektanet/advertiser_mangement/views.py
+++ django/yektanet/advertiser_mangement/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Advertiser, Ad
 
 # Create your views here.
 def index(req):
+  advertiser_array = Advertiser.objects.order_by('-name')
+  for advertiser in advertiser_array:
+    advertiser.ads = Ad.objects.filter(owner = advertiser)
   return render(req, 'advertiser_mangement/ads.html',{
-    'advertisers' : [],
+    'advertisers' : advertiser_array,
   })
