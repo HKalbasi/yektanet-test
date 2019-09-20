a751dc157a841aa73663905f0e654eb1d24b68b2
IROpen <hamid.6.11.79@gmail.com>
2019-09-20 14:58:00 +0430

    recover admin path

--- django/yektanet/yektanet/urls.py
+++ django/yektanet/yektanet/urls.py
@@ -17,5 +17,6 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('advertiser_mangement.urls')),
 ]
