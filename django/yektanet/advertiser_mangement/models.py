d1922bec1af0f6f9d3cb148eccebeae11f675a29
IROpen <hamid.6.11.79@gmail.com>
2019-09-20 13:22:18 +0430

    create models

--- django/yektanet/advertiser_mangement/models.py
+++ django/yektanet/advertiser_mangement/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Advertiser(models.Model):
+  name   = models.CharField(max_length=100, null = False)
+  clicks = models.IntegerField(default=0)
+  views  = models.IntegerField(default=0)
+
+class Ad(models.Model):
+  title  = models.CharField(max_length=100, null = False)
+  imgUrl = models.CharField(max_length=1000, null = False)
+  link   = models.CharField(max_length=1000, null = False)
+  owner  = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+  clicks = models.IntegerField(default=0)
+  views  = models.IntegerField(default=0)
