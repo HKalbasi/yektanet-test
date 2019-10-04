b95603df4e37104c6895f1a7376d6e353217bd43
IROpen <hamid.6.11.79@gmail.com>
2019-10-04 15:12:03 +0330

    use annotate to reduce database queries

--- django/yektanet/advertiser_mangement/views.py
+++ django/yektanet/advertiser_mangement/views.py
@@ -3,6 +3,7 @@ from .models import Advertiser, Ad, Click, View
 from django.views.generic.base import TemplateView, RedirectView, View as ClassView
 from datetime import datetime, timedelta
 from django.http import Http404
+from django.db.models import Count
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -58,6 +59,7 @@ class ReportView(ClassView):
       end   = datetime.strptime(req.GET['end'],"%Y-%m-%d-%H")
     except Exception as e:
       return redirect('/report/select_time/')
+    res = {}
     for ad in ads:
       clicks = Click.objects.filter(owner = ad)
       views = View.objects.filter(owner = ad)
@@ -68,21 +70,35 @@ class ReportView(ClassView):
           raise "what?"
         else:
           diff += (click.time - view.time).total_seconds()
-      tt = start
-      ad.table = []
-      while tt < end:
-        nt = tt + timedelta(hours=1)
-        cc = clicks.filter(time__lt = nt , time__gt = tt).count()
-        cv = views .filter(time__lt = nt , time__gt = tt).count()
-        ad.table.append({
-          'time'    : tt.hour,
-          'clicks'  : cc,
-          'views'   : cv,
-          'average' : cc/max(cv,1)
-        })
-        tt = nt
       ad.dis_av = diff / max(clicks.count(),1)
-      ad.rate = clicks.count() / max(views.count(),1)
+    clickCount = Click.objects.values('owner').annotate(count=Count('id'))
+    viewCount  = View .objects.values('owner').annotate(count=Count('id'))
+    for ad in ads:
+      res[ad.id] = { 'table' : [], 'title': ad.title, 'click_count': 0, 
+        'view_count': 0, 
+        'rate': '-',
+        'dis_av': ad.dis_av,
+      }
+    for cc in clickCount:
+      res[cc['owner']]['click_count'] = cc['count']
+    for vc in viewCount:
+      res[vc['owner']]['view_count'] = vc['count']
+      res[vc['owner']]['rate'] = res[cc['owner']]['click_count']/vc['count']
+    tt = start
+    while tt < end:
+      nt = tt + timedelta(hours=1)
+      cc = Click.objects.values('owner').filter(time__lt = nt , time__gt = tt).annotate(count=Count('id'))
+      cv = View .objects.values('owner').filter(time__lt = nt , time__gt = tt).annotate(count=Count('id'))
+      for ad in ads:
+        res[ad.id]['table'].append({'time': tt.hour, 'clicks': 0, 'views': 0, 'average': '-'})
+      for x in cc:
+        res[x['owner']]['table'][-1]['clicks'] = x['count']
+      for x in cv:
+        res[x['owner']]['table'][-1]['views'] = x['count']
+        res[x['owner']]['table'][-1]['average'] = res[x['owner']]['table'][-1]['clicks']/x['count']
+        
+      tt = nt
     return render (req, "advertiser_mangement/report.html",{
-      "ads": ads
+      "ads": ads,
+      "res": res,
     })
